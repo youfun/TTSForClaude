7e5bc4285699d57e4f73a9829f2c96c222f49b48
youfun <junezpe@gmail.com>
2023-08-06 18:44:26 +0800

    deploy fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,9 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite'
+
+/**使用自定义Adapter时可能需要配置 vitePreproces
+但如果你使用自定义的adapter,比如 @sveltejs/adapter-node 或者自己实现的适配器,那么就需要手动添加 vitePreprocess。这是因为自定义适配器不会做 Svelte 的预处理。
+ */
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
